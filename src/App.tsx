d33f2b2ab4992f52bdbb8aaeab1b2d875c55f4c7
rashait95 <rashabaakhras@gmial.com>
2024-06-12 01:39:00 +0300

    create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Navbar from './componnents/NavBar/Navbar'
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-     
+      <Navbar/>
       <h1>Siria Herbs Website</h1>
     </>
   )
