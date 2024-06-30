7fd350c44a57086ad286469cda87add19f98fe86
nawar <nawaralissa23@gmail.com>
2024-06-30 19:47:09 +0200

    quote

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 import './App.css'
 import React from 'react';
 //import Footer from './componnents/Footer/Footer';
-import Iso from './componnents/Iso/Iso';
+//import Iso from './componnents/Iso/Iso';
+import Qoute from './componnents/Qoute/Qoute';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <h1>Siria Herbs Website</h1>
-      <Iso />
+      <Qoute />
     </>
   )
 }
