65d25e03d9b2b9768abc1f79b4a1ef62e8dae5e3
HindAljendy <142920133+HindAljendy@users.noreply.github.com>
2024-07-02 19:54:45 +0300

    Update Hind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import './App.css'
 import React from 'react';
-
-//import Footer from './componnents/Footer/Footer';
-import Iso from './componnents/Iso/Iso';
-
-import Product_Section from './componnents/Product_section/Product_Section'
 import Navbar from './componnents/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
@@ -15,9 +10,9 @@ import GreenGold from './pages/GreenGold/GreenGold';
 import Ogaro from './pages/Ogaro/Ogaro';
 import Malika from './pages/Malika/Malika';
 import Avie from './pages/Avie/Avie';
-import NewsLetterForm from './componnents/News Letter Form/NewsLetterForm';
-import Store from './componnents/Store/History-Story';
-import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy';
+import SectionEndPage from './componnents/SectionEndPage/SectionEndPage';
+import Certifications from './componnents/Certifications/Certifications';
+
 
 
 
@@ -27,33 +22,24 @@ function App() {
 
   return (
     <>
+      <Navbar />
 
-     
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />}>
+          <Route index element={<GreenGold />} />
+          <Route path="/products/greengold" element={<GreenGold />} />
+          <Route path="/products/ogaro" element={<Ogaro />} />
+          <Route path="/products/malika" element={<Malika />} />
+          <Route path="/products/avie" element={<Avie />} />
+        </Route>
 
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/products" element={<Products/>}>
-        <Route index element={<GreenGold />} />
-        <Route path="/products/greengold" element={<GreenGold />} />
-        <Route path="/products/ogaro" element={<Ogaro />} />
-        <Route path="/products/malika" element={<Malika />} />
-        <Route path="/products/avie" element={<Avie />} />
-      </Route>
-      
-      <Route path="/contact" element={<Contact/>}/>
-    </Routes>
-    
-    <NewsLetterForm/>
-    <Product_Section/>
-    <PrivacyPolicy/>
-          <Iso />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/certifications" element={<Certifications />} />
+      </Routes>
 
-     <Store />
-
-
+      <SectionEndPage />
 
 
     </>
